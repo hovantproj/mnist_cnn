2df689f7551bf43a8b74913ea9ad831a9895c1a6
hovantproj <hovant.per@gmail.com>
2026-07-03 18:07:13 +1000

    Create file and pseudocode

---
+++ basic_cnn.py
@@ -0,0 +1,20 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+# Get dataloader
+
+# Load the dataset, 1 for training, 1 for proper testing
+
+# Prepare the transforms
+
+# Make the class for the cnn
+
+# ReLU, softmax and probably some form of optimisation
+
+# Make the training function
+
+# Find loss and other stuff
